50cde2da00848f159ecf8ec5152a1fbf81e4fb60
hyunnaye <nayeonhyun802@gmail.com>
2021-07-14 20:34:43 +0900

    fixed browser router

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import {
 function App() {
   
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
       <Switch>
       `   <Route exact path='/' component={Homepage}/>
